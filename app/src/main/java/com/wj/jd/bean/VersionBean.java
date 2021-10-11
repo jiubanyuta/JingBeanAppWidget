c930bada6b9fdbe4f6209da2d1a3f780b651fb60
wangjing <1029@qq.com>
2021-10-11 12:43:21 +0800

    1

---
+++ app/src/main/java/com/wj/jd/bean/VersionBean.java
@@ -0,0 +1,37 @@
+package com.wj.jd.bean;
+
+/**
+ * author wangjing
+ * Date 2021/10/11
+ * Description
+ */
+public class VersionBean {
+
+    private String isUpdate;
+    private String content_url;
+    private String release;
+
+    public String getIsUpdate() {
+        return isUpdate;
+    }
+
+    public void setIsUpdate(String isUpdate) {
+        this.isUpdate = isUpdate;
+    }
+
+    public String getContent_url() {
+        return content_url;
+    }
+
+    public void setContent_url(String content_url) {
+        this.content_url = content_url;
+    }
+
+    public String getRelease() {
+        return release;
+    }
+
+    public void setRelease(String release) {
+        this.release = release;
+    }
+}
