2dff02eab2c9cd635b3f867d9172cdebdd2e78b5
wangjing <1029@qq.com>
2021-10-11 14:05:13 +0800

    1

--- app/src/main/java/com/wj/jd/BaseActivity.kt
+++ app/src/main/java/com/wj/jd/BaseActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
 import com.gyf.immersionbar.ktx.immersionBar
+import com.wj.jd.dialog.NewStyleDialog
 
 /**
  * Created by Administrator on 2018/1/4.
@@ -12,11 +14,13 @@ import com.gyf.immersionbar.ktx.immersionBar
 abstract class BaseActivity : AppCompatActivity() {
     private var barView: View? = null
     private var titleTv: TextView? = null
+    private lateinit var currentActivity: FragmentActivity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(setLayoutId())
         barView = findViewById(R.id.barView)
         titleTv = findViewById(R.id.title)
+        currentActivity = this
         immersionBar {
             statusBarColor(R.color.colorPrimary)
             statusBarView(barView)
@@ -39,4 +43,63 @@ abstract class BaseActivity : AppCompatActivity() {
     fun setTitle(str: String) {
         titleTv?.text = str
     }
+
+
+    protected var dialog: NewStyleDialog? = null
+
+    open fun createDialogTip(content: String?) {
+        createDialog("温馨提示", content, "确定", null)
+    }
+
+    open fun createDialogTip(content: String?, onRightClickListener: NewStyleDialog.OnRightClickListener?) {
+        createDialog("温馨提示", content, "确定", onRightClickListener)
+    }
+
+
+    open fun createDialog(
+        title: String?,
+        content: String?,
+        rightBtn: String?,
+        onRightClickListener: NewStyleDialog.OnRightClickListener?
+    ) {
+        createDialog(title, content, "", rightBtn, null, onRightClickListener)
+    }
+
+    open fun createDialog(
+        title: String?,
+        content: String?,
+        leftBtn: String?,
+        rightBtn: String?,
+        onRightClickListener: NewStyleDialog.OnRightClickListener?
+    ) {
+        createDialog(title, content, leftBtn, rightBtn, null, onRightClickListener)
+    }
+
+    open fun createDialog(
+        title: String?,
+        content: String?,
+        leftBtn: String?,
+        rightBtn: String?,
+        onLeftClickListener: NewStyleDialog.OnLeftClickListener?,
+        onRightClickListener: NewStyleDialog.OnRightClickListener?
+    ) {
+        if (dialog != null) {
+            disMissDialog()
+        }
+        dialog = NewStyleDialog(currentActivity, title, content, leftBtn, rightBtn)
+        dialog?.apply {
+            setCancelable(false)
+            setLeftClickListener(onLeftClickListener)
+            setRightClickListener(onRightClickListener)
+            pop()
+        }
+    }
+
+    open fun disMissDialog() {
+        if (dialog != null && dialog!!.isShowing()) {
+            dialog!!.dismiss()
+            dialog = null
+        }
+    }
+
 }
