4ba1a1a219aa6d309c8ac50993fef380211321e7
wangjing <1029@qq.com>
2021-10-12 16:00:17 +0800

    正常版本

--- app/src/main/java/com/wj/jd/MainActivity.kt
+++ app/src/main/java/com/wj/jd/MainActivity.kt
@@ -26,6 +26,10 @@ import com.wj.jd.util.StringCallBack
 import com.wj.jd.widget.UpdateDataService
 import com.zhy.base.fileprovider.FileProvider7
 import java.io.File
+import android.content.ActivityNotFoundException
+
+
+
 
 class MainActivity : BaseActivity() {
     private lateinit var notificationUpdateReceiver: NotificationUpdateReceiver
@@ -39,7 +43,7 @@ class MainActivity : BaseActivity() {
     }
 
     override fun initData() {
-        checkAppUpdate()
+//        checkAppUpdate()
         startUpdateService()
         initNotification()
     }
@@ -174,6 +178,31 @@ class MainActivity : BaseActivity() {
                 inputCK.setText("")
                 startService(Intent(this, UpdateDataService::class.java))
             }
+        }
+
+        addQQGroup.setOnClickListener {
+            joinQQGroup("n5xKKCpsHU-7IfmhYguyVmYXGo8t2pGy")
+        }
+    }
+
+    /****************
+     *
+     * 发起添加群流程。群号：豆豆交流群。(908891563) 的 key 为： n5xKKCpsHU-7IfmhYguyVmYXGo8t2pGy
+     * 调用 joinQQGroup(n5xKKCpsHU-7IfmhYguyVmYXGo8t2pGy) 即可发起手Q客户端申请加群 豆豆交流群。(908891563)
+     *
+     * @param key 由官网生成的key
+     * @return 返回true表示呼起手Q成功，返回false表示呼起失败
+     */
+    fun joinQQGroup(key: String): Boolean {
+        val intent = Intent()
+        intent.data = Uri.parse("mqqopensdkapi://bizAgent/qm/qr?url=http%3A%2F%2Fqm.qq.com%2Fcgi-bin%2Fqm%2Fqr%3Ffrom%3Dapp%26p%3Dandroid%26jump_from%3Dwebapi%26k%3D$key")
+        // 此Flag可根据具体产品需要自定义，如设置，则在加群界面按返回，返回手Q主界面，不设置，按返回会返回到呼起产品界面    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        return try {
+            startActivity(intent)
+            true
+        } catch (e: java.lang.Exception) {
+            // 未安装手Q或安装的版本不支持
+            false
         }
     }
 
