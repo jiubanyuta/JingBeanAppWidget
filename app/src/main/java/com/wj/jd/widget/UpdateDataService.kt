1072e6e9a9a832a78789fa36959144d254d5bfb1
wangjing <1029@qq.com>
2021-10-13 14:41:40 +0800

    1.9

--- app/src/main/java/com/wj/jd/widget/UpdateDataService.kt
+++ app/src/main/java/com/wj/jd/widget/UpdateDataService.kt
@@ -6,22 +6,12 @@ import android.os.IBinder
 import java.util.*
 
 class UpdateDataService : Service() {
-    var timer: Timer? = null
     override fun onBind(intent: Intent): IBinder? {
         return null
     }
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-        if (timer == null) {
-            timer = Timer()
-            timer!!.schedule(object : TimerTask() {
-                override fun run() {
-//                    updata();
-                }
-            }, 0, (30 * 60 * 1000).toLong())
-        } else {
-//            updata();
-        }
+        WidgetUpdateDataUtil.updateWidget()
         return super.onStartCommand(intent, flags, startId)
     }
 }
