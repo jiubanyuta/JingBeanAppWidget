be11779b3ec9391eda5eb062fd72f76057525337
wangjing <1029@qq.com>
2021-10-14 15:47:37 +0800

    支持plus

--- app/src/main/java/com/wj/jd/MuchCkActivity.kt
+++ app/src/main/java/com/wj/jd/MuchCkActivity.kt
@@ -1,8 +1,14 @@
 package com.wj.jd
 
+import android.text.TextUtils
 import android.widget.Toast
 import com.wj.jd.util.CacheUtil
 import com.wj.jd.widget.WidgetUpdateDataUtil
+import com.wj.jd.widget.WidgetUpdateDataUtil1
+import com.wj.jd.widget.WidgetUpdateDataUtil2
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.updateCK
+import kotlinx.android.synthetic.main.activity_much.*
 import kotlinx.android.synthetic.main.activity_setting.*
 
 class MuchCkActivity : BaseActivity() {
@@ -16,8 +22,29 @@ class MuchCkActivity : BaseActivity() {
     }
 
     override fun initData() {
+        inputCK1.setText(CacheUtil.getString("ck1"))
+        inputCK1.setText(CacheUtil.getString("ck2"))
     }
 
     override fun setEvent() {
+        updateCK1.setOnClickListener {
+            if (TextUtils.isEmpty(inputCK1.text.toString())) {
+                Toast.makeText(this, "CK为空，添加失败", Toast.LENGTH_SHORT).show()
+            } else {
+                CacheUtil.putString("ck1", inputCK1.text.toString())
+                Toast.makeText(this, "CK2添加成功", Toast.LENGTH_SHORT).show()
+                WidgetUpdateDataUtil1.updateWidget("ck1")
+            }
+        }
+
+        updateCK2.setOnClickListener {
+            if (TextUtils.isEmpty(inputCK2.text.toString())) {
+                Toast.makeText(this, "CK为空，添加失败", Toast.LENGTH_SHORT).show()
+            } else {
+                CacheUtil.putString("ck2", inputCK2.text.toString())
+                Toast.makeText(this, "CK3添加成功", Toast.LENGTH_SHORT).show()
+                WidgetUpdateDataUtil2.updateWidget("ck2")
+            }
+        }
     }
 }
