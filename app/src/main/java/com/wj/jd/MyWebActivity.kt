25769072bfebf91e104cbf69dee2397ee56cdd95
wangjing <1029@qq.com>
2021-10-15 12:17:11 +0800

    小组件宽度自定义

--- app/src/main/java/com/wj/jd/MyWebActivity.kt
+++ app/src/main/java/com/wj/jd/MyWebActivity.kt
@@ -1,5 +1,8 @@
 package com.wj.jd
 
+import android.content.Context
+import android.webkit.CookieManager
+import android.webkit.CookieSyncManager
 import kotlinx.android.synthetic.main.activity_web.*
 
 class MyWebActivity : BaseActivity() {
@@ -13,9 +16,21 @@ class MyWebActivity : BaseActivity() {
     }
 
     override fun initData() {
+        removeCookie(this)
         mCommonWebView.loadUrl("https://m.jd.com")
     }
 
     override fun setEvent() {
+    }
+
+    private fun removeCookie(context: Context) {
+        try {
+            CookieSyncManager.createInstance(context)
+            val cookieManager = CookieManager.getInstance()
+            cookieManager.removeAllCookie()
+            CookieSyncManager.getInstance().sync()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
